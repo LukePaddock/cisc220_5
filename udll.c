0905e138a2da0d46ec8100f8b305b708b3eed05b
LukePaddock <14lp9@queensu.ca>
2016-11-29 21:53:13 -0500

    get() is working! (next: finish insert)

--- udll.c
+++ udll.c
@@ -2,6 +2,7 @@
    Luke Paddock (10157251)
    2016-11-29 */
 
+#include<stdlib.h>
 #include<stdio.h>
 #include"udll.h"
 
@@ -11,7 +12,7 @@ typedef struct { int *prev; int *next; union Data value; } Node;
 
 */
 
-Node cn; /* First Node */
+Node *cn; /* First Node */
 int size;
 int crnt; /* current position (index of cn) */
 
@@ -19,19 +20,23 @@ void insert(int index, union Data data)
 {
 	if (size == 0) {
 		/* We need to create the first node! */
-		cn.prev = NULL;
-		cn.next = NULL;
-		cn.value = data;
+		cn = malloc(sizeof(struct Node));
+		cn->prev = NULL;
+		cn->next = NULL;
+		cn->value = data;
 		size = 1;
 		crnt = 0;
 	}
 	else if (index < 0 || index >= size) { /* just add the node to the end */
-		while (cn.next != NULL) { 
-			cn = *cn.next;
+		while (cn->next != NULL) { 
+			cn = cn->next;
 			crnt++;
 		}
-		Node x = { &cn, NULL, data };
-		cn.next = &x;
+		Node *x = malloc(sizeof(struct Node));
+		x->prev = cn;
+		x->next = NULL;
+		x->value = data;
+		cn->next =  x;
 		size++;
 	}
 }
@@ -47,19 +52,19 @@ union Data get(int index)
 		index = ( size - 1 );
 	}
 	if (crnt > index) { 
-		cn = *cn.prev;
+		cn = cn->prev;
 		crnt = crnt - 1;
 		return get(index); 
 	}
 	else if (crnt < index) { 
-		cn = *cn.next;
+		cn = cn->next;
 		crnt = crnt + 1;
 		return get(index);
 	}
 	else { /* crnt == index */ 
-		return cn.value;
+		return cn->value;
 	}
-	return cn.value; /* put this in to stop gcc warning */
+	return cn->value; /* put this in to stop gcc warning */
 }
 
 int debug_size() { return size; }
