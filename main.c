03aa0c80dbb6147016b38fdba7dc3883d5ffffa1
LukePaddock <14lp9@queensu.ca>
2016-12-01 14:46:45 -0500

    Finished the program

--- main.c
+++ main.c
@@ -17,44 +17,41 @@
 void print_data();
 
 int main(void) {
-	for (int x = 0; x < 6; x++) {
+
+	for (char x = 'a'; x < 'g'; x++) {
 		union Data i;
-		i.a = x;
+		i.e = x;
 		list_insert(-1, i);
 	}
 
 	print_data();
-	printf("\nCreated list of 10 integers 0-9\n");
 	union Data pizza;
-	pizza.a = 13;
+	pizza.e = 'x';
+	list_insert(2, pizza);
 	list_insert(0, pizza);
-	pizza.a = 40;
-	list_insert(5, pizza);
-	pizza.a = 1234;
-	list_insert(2, pizza);
-	pizza.a = 100000;
-	list_insert(2, pizza);
-	printf("added 13 to index 0\nadded 40 at index 5\nadded 1234 at index 2\nadded 100000 at index 2\n");
+	list_insert(-1, pizza);
+	printf("inserting 'x' at index 2, 0, then the end of the list\n");
 	print_data();
 	
 	
 	list_remove(0);
 	list_remove(3);
-	list_remove(3);
+	printf("removing nodes at index 0, then 3\n");
 	print_data();
 
 }
 
 void print_data() { /* prints the linked list */
-	printf("\n----------------\n");
+	printf("----------------\nList: ");
 	int peter = list_length();
 	union Data number;
 	for ( int y = 0; y < peter; y++ ) {
 		number = list_get(y);
-		printf("%d\n", number.a);
+		printf("%c ", number.e);
 	}
+	printf("\n");
 	printf("---------------\n");
 	
-	printf(" \nSize: %d\nCrnt: %d\n", list_length(), list_currentIndex());
+	printf("Size: %d\n", list_length());
 }
 
